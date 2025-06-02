226df1e7824255a950d67f6afe2e80991bb652e9
rayann1182 <Oliveira.xavier.victor@escola.pr.gov.br>
2025-06-02 09:10:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-09-02T00:00:00");
 const tempoObjetivo2 = new Date("2026-05-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-10T00:00:00");
-const tempoObjetivo4 = new Date("2025-12-10T00:00:00");
+const tempoObjetivo4 = new Date("2027-06-06T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
